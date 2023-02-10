5fe3709acf08fcb5ca6d5fe43d405c5d9aec5641
Yassin Zaher <yassinzaherelfeliri@gmail.com>
2023-02-10 12:07:18 +0100

    refactore v1.0

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import SavedScreen from "./screens/SavedScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Hello from react native</Text>
-        <StatusBar style="auto" />
+      <View style={{ flex: 1 }}>
+        <Stack.Navigator>
+          <Stack.Group>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{ headerTitle: "Translate" }}
+            />
+            <Stack.Screen name="Notifications" component={SavedScreen} />
+            <Stack.Screen name="Profile" component={SettingsScreen} />
+          </Stack.Group>
+        </Stack.Navigator>
       </View>
     </NavigationContainer>
   );
